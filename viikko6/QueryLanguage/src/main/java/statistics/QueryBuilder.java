3e569fa65a6c6c207160dd21708b59f15149dde4
pyigyli <pyry.vahakangas@luukku.com>
2020-02-29 17:47:28 +0200

    viikon 6 tehtävät tehty

---
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -0,0 +1,36 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class QueryBuilder {
+    
+  private Matcher matcher;
+
+  public QueryBuilder() {
+    this.matcher = new All();
+  }
+
+  public Matcher build() {
+    return this.matcher;
+  }
+
+  public QueryBuilder oneOf(Matcher... matchers) {
+    this.matcher = new Or(matchers);
+    return this;
+  }
+
+  public QueryBuilder playsIn(String team) {
+    this.matcher = new And(this.matcher, new PlaysIn(team));
+    return this;
+  }
+
+  public QueryBuilder hasAtLeast(int value, String category) {
+    this.matcher = new And(this.matcher, new HasAtLeast(value, category));
+    return this;
+  }
+
+  public QueryBuilder hasFewerThan(int value, String category) {
+    this.matcher = new And(this.matcher, new HasFewerThan(value, category));
+    return this;
+  }
+}
